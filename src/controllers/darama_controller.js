ee0198e9821a84e28dface05f1be50b4dd264beb
232Ahmed232 <x99aliahmed@gmail.com>
2025-12-18 21:51:15 +0500

    added Suno chanda

--- src/controllers/darama_controller.js
+++ src/controllers/darama_controller.js
@@ -13,39 +13,38 @@ import { Producer } from "../models/producer_model.js";
 
 
 
-
 const addDarama = asyncHandeler(async (req, res) => {
   const actors = await Actor.find({
-    fullName: { $in: ["Danish Taimoor","Nabeel Zafar","Faizan Sheikh"] }
+    fullName: { $in: ["Farhan Saeed","Syed Muhammad Ahmed"] }
   });
   const Female = await Female_actor.find({
-    fullName: { $in: ["Arjumand Rahim","Nadia Afgan","Sarah Khan"] }
+    fullName: { $in: ["Iqra Aziz","Nadia Afgan","Mashal Khan"] }
   });
   const directors = await Director.find({
     fullName: { $in: ["Aehsun Talish"] }
   });
   const ost = await OST.find({
-    fullName: { $in: ["SHER  | OST"] }
+    fullName: { $in: ["Suno Chanda 2  | OST"] }
   });
   const writers = await Writer.find({
-    fullName: { $in: ["Zanjabeel Asim Shah"] }
+    fullName: { $in: ["Saima Akram Chaudhry"] }
   });
   const producers = await Producer.find({
-    fullName: { $in: ["Abdullah Seja"] }
+    fullName: { $in: ["Momina Duraid"] }
   });
 
   const drama = new Darama({
-    name: "Shair",
-    poster: "https://www.pakdramas.com/wp-content/uploads/2024/09/Shair-Drama-280x414.webp",
-    plot: "Although the specifics of the plot remain a mystery, Shair is set to delve into profound themes of love, loss, and redemption. The drama is expected to weave a tapestry of complex relationships and multifaceted characters, with a narrative full of unexpected twists and emotional depth. With its promise of dramatic intensity and heartfelt storytelling, Shair is poised to captivate audiences and leave a lasting impression.",
+    name: "Suno Chanda season 2",
+    poster: "https://upload.wikimedia.org/wikipedia/en/0/0e/Suno-chanda.jpg",
+    plot: "Arsal and Jiya are childhood companions who play pranks on each other. Bi Jaan decides to get Arsal and Jiya's formal wedding and rukhsati done to honour her husband's last wish. Arsal and Jiya are against the marriage plan and team up with Kinza, Masooma's daughter, to stop the wedding.Shah Jahan, Bi Jaan's brother-in-law, and his grandson Shehryar, who lives in London, visit Pakistan for Arsal and Jiya's planned wedding after Eid. Sherry is close to his cousins and a co-conspirator in their plots to stop the marriage. Sherry asks Arsal and Jiya to resolve their problem amicably. Sherry also helps Jiya fulfil her dream of joining the London School of Economics, and they plan to study together. Sherry eventually develops feelings for Jiya, which makes Arsal jealous.Arsal and Jiya create misunderstandings between their parents. Meanwhile, Sherry falls in love with Jiya. Arsal and Jiya's parents want Arsal to divorce Jiya, but Arsal denies it. Jiya's father sends a divorce notice to Arsal, who then confesses his feelings to Jiya.Jiya initially rejects Arsal but eventually realises her love for him.Later, the family discovers that Arsal and Jiya have fallen in love. Arsal and Jiya's parents eventually give in to their love, and the wedding preparations begin. Sherry and Kinza also start liking each other.Jiya and Arsal argue again on their wedding night, but later realise their foolishness and say sorry to each other. They promise not to repeat the same mistakes and to confess their love for each other.",
     ost: ost.map(os => os._id),
-    year: 2025,
-    channel: "ARY",
-    noOfEpisodes: 39,
+    year: 2019,
+    channel: "HUM",
+    noOfEpisodes: 30,
     writers: writers.map(writer => writer._id),
     directors: directors.map(director => director._id),
     producers: producers.map(producer => producer._id),
-    genres: ["Drama","Romance","Action"],
+    genres: ["Drama","Romance","Comedy"],
     actors: actors.map(actor => actor._id), // ✅ multiple actors
     Female_actors: Female.map(Fem => Fem._id), // ✅ multiple actors
   });
